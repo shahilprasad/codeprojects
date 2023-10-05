10ed05fb22ef55a91527392590ae0b5873ba396a
shahilprasad <shahil.prasad17s@gmail.com>
2023-10-05 13:07:30 +1100

    Updated lesson on python intro.

--- python/intro/hello.py
+++ python/intro/hello.py
@@ -1,3 +1,3 @@
-print('What is your name?')
-ny_name = input()
-print('Hello, ' + ny_name)
+my_name = input('What is your name? ')
+my_age = int(input('What is your age? '))
+print(f'Hello, {my_name} you are {my_age + 10} years old in 10 years.')
