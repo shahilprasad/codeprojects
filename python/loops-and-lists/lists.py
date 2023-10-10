c6a5104eb9209bb236146f8098772f95f848e10b
shahilprasad <shahil.prasad17s@gmail.com>
2023-10-10 13:53:11 +1100

    Python lessons on lists and loops, and some previous lessons.

---
+++ python/loops-and-lists/lists.py
@@ -0,0 +1,11 @@
+spam = ['cat', 'dog', 'bird']
+eggs = [14, 15, 16, 17, 18]
+foo = ['Shahil', 24]
+tic_tac_toe = [
+    ['', 'O', ''],
+    ['X', 'O', ''],
+    ['', 'X', 'O', '']
+]
+
+spam[1] = 'hello'
+print(spam)
