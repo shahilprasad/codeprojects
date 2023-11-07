232b4b8b5f7a04db82a692b927ef640e6e9cb0c5
shahilprasad <shahil.prasad17s@gmail.com>
2023-11-07 18:54:25 +1100

    Added data to table.

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,16 +1,24 @@
-drop table categories;
 drop table items;
+drop table categories;
 
 create table categories (
     id serial primary key,
+
     name varchar(50) not null,
     description text
 );
 
 create table items (
     id serial primary key,
+
     name varchar(200) not null,
     description text not null,
+
     category_id integer not null
     foreign key (category_id) references categories (id)
 );
+
+insert into categories (name, description) values
+    ('Electronics', 'Gadgets to make life easier'),
+    ('Car Parts', 'Getting from point A to B')
+    ;
