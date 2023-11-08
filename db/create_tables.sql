3c7f46418aed0add11bb738546e9eeb038c6ebde
shahilprasad <shahil.prasad17s@gmail.com>
2023-11-08 14:00:11 +1100

    Updated create_tables sql with data and created new weather file.

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -4,9 +4,14 @@ drop table categories;
 create table categories (
     id serial primary key,
 
-    name varchar(50) not null,
+    name varchar(50) not null unique,
     description text
 );
+
+insert into categories (name, description) values
+    ('Electronics', 'Gadgets to make life easier'),
+    ('Car Parts', 'Getting from point A to B')
+    ;
 
 create table items (
     id serial primary key,
@@ -14,11 +19,13 @@ create table items (
     name varchar(200) not null,
     description text not null,
 
-    category_id integer not null
+    category_id integer not null,
     foreign key (category_id) references categories (id)
 );
 
-insert into categories (name, description) values
-    ('Electronics', 'Gadgets to make life easier'),
-    ('Car Parts', 'Getting from point A to B')
+insert into items (name, description, category_id) values
+    ('Skyrim', 'Open-world RPG', 1),
+    ('Fifa', 'Sports game about football', 1),
+    ('Wheels', 'Go round and round', 2),
+    ('Tint', 'Reduce UV and brightness', 2)
     ;
